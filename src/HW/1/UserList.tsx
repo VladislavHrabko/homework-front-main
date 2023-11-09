d51d825c01aa233ba4a0c009db8a72b3ca5870ba
VladislavHrabko <velbel92@mail.ru>
2023-11-09 16:25:18 +0300

    HW1

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -18,10 +18,10 @@ export const UserList = (props: UserListPropsType) => {
       <h2>User List:</h2>
 
       <ul>
-        {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
-          <li key={user.id} id={`hw01-user-${user.id}`}>
-            <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
-            {user.address.street}, {user.address.city}
+        {props.users.map((el) => (// ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
+          <li key={el.id} id={`hw01-user-${el.id}`}>
+            <strong>{el.name}</strong> (Age: {el.age})<strong> Address:</strong>
+            {el.address.street}, {el.address.city}
           </li>
         ))}
       </ul>
